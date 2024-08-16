020d4e2fd11a067480fcc809c14658739749c44a
Antor4q <tariquelislam2015@gmail.com>
2024-08-16 12:28:07 +0600

    producs geting api created

--- index.js
+++ index.js
@@ -7,11 +7,53 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i8hseoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productsCollection =client.db("echoMartDB").collection("products")
+
+    app.get("/products", async(req,res)=>{
+        console.log(req.body,"this is body")
+        console.log(req.query,"this is query")
+        const result = await productsCollection.find().toArray()
+        res.send(result)
+    })
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// dfsdfd
+
 app.get("/", (req,res)=>{
     res.send("Echo Mart running now")
 })
 
-// dfsdfd
 app.listen(port, ()=>{
     console.log(`Echo mart running on port ${port}`)
 })
